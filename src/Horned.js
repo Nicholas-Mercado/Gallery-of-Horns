2043dd954c7669eecf7c0d5d025570d28d8518b8
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-14 17:33:27 -0800

    Created three components for horned animals

--- src/Horned.js
+++ src/Horned.js
@@ -3,7 +3,14 @@ import React from "react";
 class HornedBeast extends React.Component{
   render(){
     return(
-      <h4>test</h4>
+      <>
+        <article> 
+          <h2>{this.props.name}</h2>
+          <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}></img>
+          {/* Brady Camp helped me with image syntax */}
+          <p>{this.props.description}</p>
+        </article>
+      </>
     )
   }
 }
