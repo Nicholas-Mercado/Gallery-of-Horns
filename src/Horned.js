427d90e5d0927b20a8ee39a00cec117cdd48dec3
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-15 14:33:27 -0800

    Completed feature #2

--- src/Horned.js
+++ src/Horned.js
@@ -2,29 +2,25 @@ import React from "react";
 import './Horned.css';
 
 class HornedBeast extends React.Component{
-  // how to setup state component
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     waves: 0, we dont need this
-  //   }
-  // }
+  constructor(props){
+    super(props);
+    this.state = {
+     heart: 0,
+    }
+  }
 
-  //   handleWaves = () => {
-  //     this.setState({
-  //       waves: this.state.waves + 1,
-  //     })
-  //   
-  render(){
-
+    handleWaves = () => {
+      this.setState({
+        heart: this.state.heart + 1,
+      })
+    }
+  render() {
     return(
       <>
         <article> 
-        
           <h2>{this.props.title}</h2>
-          {/* onClick={this.handleWaves}  */}
-          {/* <p>emoji{this.state.waves)</p> */}
-          <img src={this.props.image_url} alt={this.props.description} title={this.props.title}></img>
+          <img onClick={this.handleWaves} src={this.props.image_url} alt={this.props.description} title={this.props.title}></img>
+          <p>❤️:{this.state.heart}</p>
           <p>{this.props.description}</p>
         </article>
       </>
--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,19 @@
 import React from "react";
 import HornedBeast from "./Horned";
-// import data from './data.json';
 import './Main.css';
 
 class Main extends React.Component{
   render(){
     let finalData = [];
-    this.props.data.forEach(hornsB =>{
+    this.props.data.forEach((hornsB, index) =>{
       finalData.push (
         <HornedBeast
+          key={index}
           title={hornsB.title}
           image_url={hornsB.image_url}
           description={hornsB.description}
+          keyword={hornsB.keyword}
+          horns={hornsB.horns}
           />
       )
     })
