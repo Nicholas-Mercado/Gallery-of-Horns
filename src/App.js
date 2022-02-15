296c731bb79f1790bfa1a12c3c68fd5dc9ef3fae
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-14 21:42:49 -0800

    added ;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './App.css';
-import Header from './Header.js'
-import Footer from './Footer.js'
-import Main from './Main.js'
+import Header from './Header.js';
+import Footer from './Footer.js';
+import Main from './Main.js';
 
 class App extends React.Component{
   render(){
