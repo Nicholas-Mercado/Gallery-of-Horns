470ceb796588fd00c50d4d3ea38eecf098270bb3
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-15 15:07:31 -0800

    Completed feature # 3

--- src/Horned.js
+++ src/Horned.js
@@ -1,28 +1,31 @@
 import React from "react";
 import './Horned.css';
+import Card from 'react-bootstrap/Card';
 
-class HornedBeast extends React.Component{
-  constructor(props){
+class HornedBeast extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-     heart: 0,
+      heart: 0,
     }
   }
 
-    handleWaves = () => {
-      this.setState({
-        heart: this.state.heart + 1,
-      })
-    }
+  handleWaves = () => {
+    this.setState({
+      heart: this.state.heart + 1,
+    })
+  }
   render() {
-    return(
+    return (
       <>
-        <article> 
-          <h2>{this.props.title}</h2>
-          <img onClick={this.handleWaves} src={this.props.image_url} alt={this.props.description} title={this.props.title}></img>
-          <p>❤️:{this.state.heart}</p>
-          <p>{this.props.description}</p>
-        </article>
+        <Card style={{ width: '40rem' }}>
+          <Card.Img onClick={this.handleWaves} src={this.props.image_url} alt={this.props.description} title={this.props.title} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>❤️:{this.state.heart}</Card.Text>
+            <Card.Text>{this.props.description}</Card.Text>
+          </Card.Body>
+        </Card>
       </>
     )
   }
