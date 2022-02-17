616ebc81bf7fe446e85b056bbba0ed2bccca9d02
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-17 15:32:32 -0800

    added filter and passed data and method

--- src/Main.js
+++ src/Main.js
@@ -3,17 +3,16 @@ import HornedBeast from "./HornedBeast";
 import './Main.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Filter from './Filter'
 
 class Main extends React.Component{
   render(){
-    // let finalData = [];
-    // this.props.data.forEach((hornsB, index) =>{
-    //   finalData.push (
-    //   )
-    // })
-
-    return(
-      <main >
+   return(
+    <main >
+      <Filter
+        data={this.props.data}
+        handleSelect={this.props.handleSelect}
+      />
       <Row  xs={1} md={2} lg={3}>
         {this.props.data.map((hornsB,index) => (
           <Col key={index}>
@@ -25,7 +24,7 @@ class Main extends React.Component{
         )
         )}
       </Row>
-      </main>
+    </main>
     )
   }
 }
